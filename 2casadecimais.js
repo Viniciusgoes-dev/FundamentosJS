5f28db4ef87f4d0ea5a0764078f96d337c39f433
Viniciusgoes-dev <viniciusgoes.dev@gmail.com>
2022-05-16 16:19:49 -0300

    Update 2casadecimais.js

--- 2casadecimais.js
+++ 2casadecimais.js
@@ -7,4 +7,4 @@ console.log(a.toString(2)); // esse comando mostra variaveis em binario
 
 console.log(typeof media) // mostra tipo de variavel
 
-console.log(Number.isInteger(peso1)) // esse comando mostra se é verdadeiro numero inteiro ou é falso numero quebrado
+console.log(Number.isInteger(peso1)) // esse comando mostra se é verdadeiro o numero inteiro ou é falso numero quebrado
